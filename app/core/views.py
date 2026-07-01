a887889d12280e07a936cd037356d4be8653050c
Scarlowxxx <rubchienko80@mail.ru>
2026-07-01 13:44:17 +0300

    the final look of our website

--- app/core/views.py
+++ app/core/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def settings(request):
     return render(request, "settings.html")
+
+def servers(request):
+    return render(request, "servers.html")
+
+def magazineHub(request):
+    return render(request, "magazineHub.html")
+
+def api(request):
+    return render(request, "api.html")
