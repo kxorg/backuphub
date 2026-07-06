08fa9f55e3b6cfa612670c434da59d0454b5feb8
Scarlowxxx <rubchienko80@mail.ru>
2026-07-06 16:32:33 +0300

    Details for viewing the system and host were added, as well as an icon for the site

--- app/config/urls.py
+++ app/config/urls.py
@@ -41,12 +41,14 @@ urlpatterns = [
     # TargetSystems CRUD 
     path('target-systems/', views.system_settings, name="target_system_list"),
     path('target-systems/create/', views.system_create, name="target_system_create"),
+    path('target-systems/<int:pk>/', views.system_detail, name="target_system_detail"),
     path('target-systems/<int:pk>/edit/', views.system_edit, name="target_system_edit"),
     path('target-systems/<int:pk>/delete/', views.system_delete, name="target_system_delete"),
     
     # Hosts CRUD 
     path('hosts/', views.servers, name="host_list"),
     path('hosts/create/', views.host_create, name="host_create"),
+    path('hosts/<int:pk>/', views.host_detail, name="host_detail"),
     path('hosts/<int:pk>/edit/', views.host_edit, name="host_edit"),
     path('hosts/<int:pk>/delete/', views.host_delete, name="host_delete"),
     
