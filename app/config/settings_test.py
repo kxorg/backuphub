ba8029276d285e9c596f11d2c35578a2ca9e3a7a
Volkov Aleksandr <sanya005volkov@mail.ru>
2026-07-07 13:07:56 +0300

    test: use PostgreSQL for pytest database

--- app/config/settings_test.py
+++ app/config/settings_test.py
@@ -1,15 +1,26 @@
+import os
+
 from .settings import *
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "TEST": {
+            "NAME": os.getenv("POSTGRES_TEST_DB", f"test_{os.getenv('POSTGRES_DB')}"),
+        },
     }
 }
+
 
 class DisableMigrations:
     def __contains__(self, item):
         return True
+
     def __getitem__(self, item):
         return None
 
