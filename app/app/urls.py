2411d53afb1bd6a5b5ab8301dae4a8073499595b
stasbuldakov <kalekakek.ken@yandex.ru>
2026-07-16 16:50:58 +0300

    live refreshing details of backup and fixed bug when i cant move to configuration at backup operations page

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from api.views import api_ui_refresh_dashboard, api_ui_refresh_operations
+from api.views import api_ui_refresh_dashboard, api_ui_refresh_operations, api_ui_refresh_operation_detail
 from app.views import index
 
 
@@ -42,4 +42,5 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/ui/refresh-dashboard/', api_ui_refresh_dashboard, name='api_ui_refresh_dashboard'),
     path('api/ui/refresh-operations/', api_ui_refresh_operations, name='api_ui_refresh_operations'),
+    path('api/ui/refresh-operation/<int:pk>/', api_ui_refresh_operation_detail, name='api_ui_refresh_operation_detail'),
 ]
